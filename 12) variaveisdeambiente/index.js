77e1db778fd9c87a02c6431a966c45b6ca002cd1
Micilini <sdanz@hotmail.com>
2024-09-28 11:33:26 -0300

    Lição .ENV

---
+++ 12) variaveisdeambiente/index.js
@@ -0,0 +1,9 @@
+// Carrega o arquivo .env
+require('dotenv').config();
+
+// Acessando as variáveis
+console.log(process.env.NOME);         // micilini
+console.log(process.env.RANK);         // 100
+console.log(process.env.SECRET);       // Micilini é DEZ!
+console.log(process.env.KEY);          // @090OALK''JJ3
+console.log(process.env.SITE_APP);     // https://micilini.com/
